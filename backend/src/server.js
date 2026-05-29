d1f3f3df55e0e87aaa76d2d6522a326e5d1cc55d
GuilhermeLuis242 <guilherme.rodrigues@ecenture.com.br>
2026-05-29 08:56:48 -0300

    correct issues in CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
 import { connectDB } from "./config/db.js";
-import { swaggerDocs } from "./config/swagger.js";
-import routes from "./routes/index.js";
+import app from "./app.js";
 
-const app = express();
 const { PORT, NODE_ENV } = process.env;
-
-app.use(express.json());
-
 
 async function startServer() {
     try {
         await connectDB();
-        app.use(`/${NODE_ENV}/api`, routes);
-        swaggerDocs(app);
         app.listen(PORT, () => {
             console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
         });
@@ -26,7 +18,4 @@ async function startServer() {
     }
 }
 
-// Inicia o servidor
 startServer();
-
-export default app;
